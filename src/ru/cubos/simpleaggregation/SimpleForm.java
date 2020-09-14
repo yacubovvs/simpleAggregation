68e11fae0b01f5bead1eef7dd08bbd3eb221a558
Vitaly <yacubovvs@yandex.ru>
2020-09-14 22:25:34 +0300

    some fixs

--- src/ru/cubos/simpleaggregation/SimpleForm.java
+++ src/ru/cubos/simpleaggregation/SimpleForm.java
@@ -32,17 +32,21 @@ public class SimpleForm extends ScannerJFrame {
         setTextSize(cargoLabel, Settings.CARGO_LABEL_TEXT_SIZE);
         setTextSize(itemLabel, Settings.ITEM_LABEL_TEXT_SIZE);
         setTextSize(totalInCargoLabel, Settings.TOTAL_LABEL_TEXT_SIZE);
+        //itemCode.setMaximumSize(new Dimension(getWidth()-10, 20));
 
         mainPanel.setBackground(new Color(Settings.BACKGROUND_COLOR[0],Settings.BACKGROUND_COLOR[1],Settings.BACKGROUND_COLOR[2]));
 
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+        onScan("00123456789012345678");
+        onScan("<html>01046070078211312100000000000591ee0592ek0v49b3v4t4r82pamitz5857i=</html>");
     }
 
     void setTextSize(JLabel jLabel, int fontSize){
         Font labelFont = jLabel.getFont();
-        jLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSize));
+        jLabel.setFont(new Font(labelFont.getName(), Font.BOLD, fontSize));
     }
 
     public static void main(String[] args) {
